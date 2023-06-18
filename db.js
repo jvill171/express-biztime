dd3728e6000c993f942c284e76977ff787a30b25
jvill171 <juanvillarreal64@yahoo.com>
2023-06-17 21:06:26 -0500

    Created db.js to connect to DB

--- db.js
+++ db.js
@@ -1,2 +1,14 @@
 /** Database setup for BizTime. */
 
+
+const { Client } = require("pg");
+
+let db = new Client({
+    host: "/var/run/postgresql/",
+    database: "biztime"
+  });
+
+  db.connect();
+
+
+module.exports = db;
