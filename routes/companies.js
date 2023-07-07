5c21594c77cfabab3838e3d787fdddb38c04f749
jvill171 <juanvillarreal64@yahoo.com>
2023-07-07 00:37:41 -0500

    Cleaned up code & added slugify

--- routes/companies.js
+++ routes/companies.js
@@ -1,9 +1,10 @@
 /** Routes for companies. */
 
-
 const express = require("express");
 const ExpressError = require("../expressError")
 const db = require("../db");
+
+const slugify = require("slugify")
 
 let router = new express.Router();
 
@@ -16,10 +17,9 @@ router.get("/", async function (req, res, next) {
            FROM companies 
            ORDER BY name`
     );
-
-    return res.json({"companies": result.rows});
+    const companies = result.rows
+    return res.json({ companies });
   }
-
   catch (err) {
     return next(err);
   }
@@ -31,14 +31,18 @@ router.get("/", async function (req, res, next) {
 router.get("/:code", async function (req, res, next) {
   try {
     const result = await db.query(
-          `SELECT * FROM companies 
+          `SELECT code,
+                  name,
+                  description
+           FROM companies 
            WHERE code = $1`, [req.params.code]
     );
 
     if(result.rows.length === 0){
       throw new ExpressError(`Company not found: ${req.params.code}`, 404)
     }
-    return res.json({"company": result.rows[0]});
+    const company = result.rows[0]
+    return res.json({ company });
   }
 
   catch (err) {
@@ -48,17 +52,26 @@ router.get("/:code", async function (req, res, next) {
 
 
 
-// **POST /companies :** Adds a company. Needs to be given JSON like: `{code, name, description}` Returns obj of new company:  `{company: {code, name, description}}`
+/**POST /companies :Adds a company. 
+ * Needs to be given JSON like: `{code, name, description}`
+ * Returns obj of new company:  `{company: {code, name, description}}`
+ * */
 router.post("/", async function (req, res, next) {
   try {
-    let {code, name, description} = req.body
+    let {name, description} = req.body
+    if(!(name && description)){
+      throw new ExpressError("Bad Request: Missing Data ", 404)
+    }
+
+    let code = slugify(name, { lower: true, strict:true })
     const result = await db.query(
           `INSERT INTO companies (code, name, description)
            VALUES($1, $2, $3)
            RETURNING code, name, description`
            , [code, name, description]
     );
-    return res.status(201).json({"company": result.rows[0]});
+    const company = result.rows[0]
+    return res.status(201).json({ company });
   }
 
   catch (err) {
@@ -71,6 +84,9 @@ router.post("/", async function (req, res, next) {
 router.put("/:code", async function (req, res, next) {
   try {
     let {name, description} = req.body
+    if(!(name && description)){
+      throw new ExpressError(`Bad Request: Missing data`, 400)
+    }
     const result = await db.query(
           `UPDATE companies
           SET name=$2, description=$3
@@ -82,8 +98,8 @@ router.put("/:code", async function (req, res, next) {
     if(result.rows.length === 0){
       throw new ExpressError(`Company not found: ${req.params.code}`, 404)
     }
-
-    return res.json({"company": result.rows[0]});
+    const company = result.rows[0]
+    return res.json({ company });
   }
 
   catch (err) {
