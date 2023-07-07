b0a556ceaaef8f048d68c3b5b742bbe8ff517a69
jvill171 <juanvillarreal64@yahoo.com>
2023-07-07 00:35:13 -0500

    Teaked code to use testing DB when process.env.NODE_ENV = 'test'

--- db.js
+++ db.js
@@ -3,12 +3,18 @@
 
 const { Client } = require("pg");
 
-let db = new Client({
+let db;
+if(process.env.NODE_ENV === "test"){
+  db = new Client({
+    host: "/var/run/postgresql/",
+    database: "biztime-test"})
+} else{
+  db = new Client({
     host: "/var/run/postgresql/",
     database: "biztime"
   });
-
-  db.connect();
+}
+db.connect();
 
 
 module.exports = db;
